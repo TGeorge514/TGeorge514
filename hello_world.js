dbf21b2f10940d508ad974c769457e6084ce889f
Jo Professional <t.i.georgetg@gmail.com>
2026-01-17 13:39:29 -0500

    Adding a JS file to modify the HTML

---
+++ hello_world.js
@@ -0,0 +1,9 @@
+const h1 = document.querySelector('h1');
+
+h1.style.textDecoration = 'underline';
+h1.style.fontFamily = 'Arial, sans-serif'; 
+h1.style.textAlign = 'center';
+
+document.querySelector('blockquote').style.fontWeight = 'bold';
+document.querySelector('blockquote p').style.fontSize = '2em';
+document.querySelector('blockquote footer').style.fontStyle = 'italic';
